bbdc00cb7dd888b1dcfe8d8f8f3b146ccceee9e9
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@siili.com>
2023-12-01 15:42:59 +0200

    clean

--- src/aoc_2023/day01.clj
+++ src/aoc_2023/day01.clj
@@ -3,11 +3,10 @@
 
 (def input (s/split-lines (slurp "resources/day1.txt")))
 
-(def input ["1abc2"
-            "pqr3stu8vwx"
-            "a1b2c3d4e5f"
-            "treb7uchet"
-            ])
+#_(def input ["1abc2"
+              "pqr3stu8vwx"
+              "a1b2c3d4e5f"
+              "treb7uchet"])
 
 (defn extract-number [s]
   (when (not-empty s)
@@ -15,25 +14,20 @@
      (let [digits (s/join (filter #(Character/isDigit %) s))]
        (str (first digits) (last digits))))))
 
-(extract-number "")
-
-;; classic version
+;; classic
 (defn solve-classic [i] 
   (reduce + (map (fn [line]
                    (Integer/parseInt
                     (let [digits (s/join (filter #(Character/isDigit %) line))]
-                      (str (first digits) (last digits)))))
+                      (str (first digits) (last digits))))) 
                  i)))
 
-(solve-classic input)
-
-(solve-classic input)
-
 ;; cool style
-(->> input
-     (map seq)
-     (map extract-number)
-     (reduce +))
+(defn solve-cool [i]
+  (->> i
+       (map seq)
+       (map extract-number)
+       (reduce +)))
 
-
-#_(apply str (filter #(Character/isDigit %) (first input)))
+(solve-classic input)
+(solve-cool input) 
